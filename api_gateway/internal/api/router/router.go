832a8f4fd2a79c44409d4434283143f7d21fa3c9
Your Name <dostonxoshimov2005@gmail.com>
2024-08-28 05:24:45 +0500

    finished

--- api_gateway/internal/api/router/router.go
+++ api_gateway/internal/api/router/router.go
@@ -5,11 +5,14 @@ import (
 	"api/internal/connections"
 	_ "api/internal/docs"
 	jwttoken "api/internal/jwt"
+	"context"
 	"crypto/tls"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	httpSwagger "github.com/swaggo/http-swagger"
 )
@@ -28,7 +31,7 @@ func NewRouter() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
 	r := http.NewServeMux()
 	handler := connections.NewHandler()
-	rate:=connections.NewRateLimiting()
+	rate := connections.NewRateLimiting()
 
 	// Users
 
@@ -43,27 +46,27 @@ func NewRouter() {
 
 	// Hotel
 
-	r.HandleFunc("POST /hotels/create",jwttoken.JWTMiddleware(rate.Limit(handler.CreateHotel)))
-	r.HandleFunc("POST /hotels/rooms/create",jwttoken.JWTMiddleware(rate.Limit(handler.CreateRoom)))
-	r.HandleFunc("GET /hotels",jwttoken.JWTMiddleware(rate.Limit(handler.GetHotels)))
-	r.HandleFunc("GET /hotels/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.GetHotel)))
-	r.HandleFunc("GET /hotels/rooms/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.GetRooms)))
-	r.HandleFunc("GET /hotels/room",jwttoken.JWTMiddleware(rate.Limit(handler.GetRoom)))
-	r.HandleFunc("PUT /hotels/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.UpdateHotel)))
-	r.HandleFunc("PUT /hotels/rooms/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.UpdateRoom)))
-	r.HandleFunc("DELETE /hotels/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.DeleteHotel)))
-	r.HandleFunc("DELETE /hotels/rooms/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.DeleteRoom)))
+	r.HandleFunc("POST /hotels/create", jwttoken.JWTMiddleware(rate.Limit(handler.CreateHotel)))
+	r.HandleFunc("POST /hotels/rooms/create", jwttoken.JWTMiddleware(rate.Limit(handler.CreateRoom)))
+	r.HandleFunc("GET /hotels", jwttoken.JWTMiddleware(rate.Limit(handler.GetHotels)))
+	r.HandleFunc("GET /hotels/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.GetHotel)))
+	r.HandleFunc("GET /hotels/rooms/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.GetRooms)))
+	r.HandleFunc("GET /hotels/room", jwttoken.JWTMiddleware(rate.Limit(handler.GetRoom)))
+	r.HandleFunc("PUT /hotels/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.UpdateHotel)))
+	r.HandleFunc("PUT /hotels/rooms/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.UpdateRoom)))
+	r.HandleFunc("DELETE /hotels/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.DeleteHotel)))
+	r.HandleFunc("DELETE /hotels/rooms/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.DeleteRoom)))
 
 	//Booking
 
-	r.HandleFunc("POST /bookings",jwttoken.JWTMiddleware(rate.Limit(handler.CreateBooking)))
-	r.HandleFunc("POST /waitinglists",jwttoken.JWTMiddleware(rate.Limit(handler.CreateWaiting)))
-	r.HandleFunc("GET /bookings/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.GetBooking)))
-	r.HandleFunc("GET /waitinglists/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.GetWaiting)))
-	r.HandleFunc("PUT /bookings/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.UpdateBooking)))
-	r.HandleFunc("PUT /waitinglists/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.UpdateWaiting)))
-	r.HandleFunc("DELETE /bookings/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.DeleteBooking)))
-	r.HandleFunc("DELETE /waitinglists/{id}",jwttoken.JWTMiddleware(rate.Limit(handler.DeleteWaiting)))
+	r.HandleFunc("POST /bookings", jwttoken.JWTMiddleware(rate.Limit(handler.CreateBooking)))
+	r.HandleFunc("POST /waitinglists", jwttoken.JWTMiddleware(rate.Limit(handler.CreateWaiting)))
+	r.HandleFunc("GET /bookings/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.GetBooking)))
+	r.HandleFunc("GET /waitinglists/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.GetWaiting)))
+	r.HandleFunc("PUT /bookings/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.UpdateBooking)))
+	r.HandleFunc("PUT /waitinglists/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.UpdateWaiting)))
+	r.HandleFunc("DELETE /bookings/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.DeleteBooking)))
+	r.HandleFunc("DELETE /waitinglists/{id}", jwttoken.JWTMiddleware(rate.Limit(handler.DeleteWaiting)))
 
 	tlsConfig := &tls.Config{
 		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
@@ -73,10 +76,26 @@ func NewRouter() {
 		Handler:   r,
 		TLSConfig: tlsConfig,
 	}
+	go GracefulShutdown(srv,logger)
 	fmt.Printf("Server started on port %s\n", c.User.Port)
 	err := srv.ListenAndServeTLS("./tls/localhost.pem", "./tls/localhost-key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
 
-  
+func GracefulShutdown(srv *http.Server, logger *slog.Logger) {
+	shutdownCh := make(chan os.Signal, 1)
+	signal.Notify(shutdownCh, os.Interrupt, os.Kill)
+
+	<-shutdownCh
+	logger.Info("Shutdown signal received, initiating graceful shutdown...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		logger.Error("Server shutdown error: " + err.Error())
+	} else {
+		logger.Info("Server gracefully stopped")
+	}
+}
